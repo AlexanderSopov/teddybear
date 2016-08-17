37140c890145dd21e97d130f665d881ea1779672
Alexander Sopov <sopovalexander@gmail.com>
2016-08-17 21:45:06 +0200

    reworked basic structure of site, content is now being dynamically loaded

---
+++ js/pages.js
@@ -0,0 +1,115 @@
+window.pages = [
+	{
+		name:"Home",
+		headline:{
+			content:"Teddybear Studio",
+			type:"h1"
+		},
+		body:[{
+				content:"Fullstack Web Developers",
+				type:"h2"
+			},{
+				content:"Do you need a developer but don't have the time to find one? Don't worry, there's no need to go through the hassle and hope for the best.",
+				type:"p",
+			},{
+				content:"Instead, give us a call. We'll be there in no time.",
+				type:"p",
+		}],
+		img:{
+			alt:"Logo of a Teddybear",
+			src:"img/teddy-1.png"
+		}
+	},
+
+
+	{
+		name:"Product",
+		headline:{
+			content:"What We Produce",
+			type:"h1"
+		},
+		body:[{
+				content:"The Worlds Fastest Websites",
+				type:"h2"
+			},{
+				content:"The web is filled with fancy stuff for fancy people.",
+				type:"p",
+			},{
+				content:"Fortunately for us, we don't need to be this pretentious. Get a static website and be the fastest guy on the web!",
+				type:"p",
+		}],
+		img:{
+			alt:"Logo of a Teddybear",
+			src:"img/teddy-1.png"
+		}
+	},
+
+
+
+	{
+		name:"Promise",
+		headline:{
+			content:"A Single Promise",
+			type:"h1"
+		},
+		body:[{
+				content:"Fastest websites in your industry",
+				type:"h2"
+			},{
+				content:"We like honest people and we want to be honest with you: a static website is not for everyone.",
+				type:"p",
+			},{
+				content:"Are you >not< everyone? Great, get in touch and we'll help you out.",
+				type:"p",
+		}],
+		img:{
+			alt:"Logo of a Teddybear",
+			src:"img/teddy-1.png"
+		}
+	},
+
+
+	{
+		name:"Partners",
+		headline:{
+			content:"Teddybear Studio",
+			type:"h1"
+		},
+		body:[{
+				content:"Fullstack Web Developers",
+				type:"h2"
+			},{
+				content:"Do you need a developer but don't have the time to find one? Don't worry, there's not need to go through the hassle and hope for the best.",
+				type:"p",
+			},{
+				content:"Instead, give us a call. We'll be there in no time.",
+				type:"p",
+		}],
+		img:{
+			alt:"Logo of a Teddybear",
+			src:"img/teddy-1.png"
+		}
+	},
+
+	{
+		name:"Contact",
+		headline:{
+			content:"Teddybear Studio",
+			type:"h1"
+		},
+		body:[{
+				content:"Fullstack Web Developers",
+				type:"h2"
+			},{
+				content:"Do you need a developer but don't have the time to find one? Don't worry, there's not need to go through the hassle and hope for the best.",
+				type:"p",
+			},{
+				content:"Instead, give us a call. We'll be there in no time.",
+				type:"p",
+		}],
+		img:{
+			alt:"Logo of a Teddybear",
+			src:"img/teddy-1.png"
+		}
+	}
+];
--- js/script.js
+++ js/script.js
@@ -1,29 +1,63 @@
+window.vrb = {};
 $(document).ready(function(){
-
-	var activeTab = "auroria";
-
-	//solves a bug in bootstrap when using .fade on tabs
-	$('#myTabs a:last').tab("show");
-	setTimeout(function(){
-		$('#myTabs a:first').tab("show");
-	},150);
-
-	// Change between the different models of beds in the tab-menu
-	$('#myTabs a').click(function (e) {
-		activeTab = $(this).attr("aria-controls");
-	});
-
-	// Change the different colors in 
-	$(".colorThumbs button").click(function (e){
-		var animationTime = 240,
-		path = "img/"+ activeTab +"/" + this.id + "-md.png",
-		elt = "#"+activeTab+"BigPic";
-
-		$(elt).fadeOut(animationTime);
-		setTimeout(function(){
-			$(elt).attr("src", path);
-			$(elt).fadeIn(animationTime);	
-		},animationTime);
-	})
-
+	vrb.headline = $("#headline"), vrb.body = $("#text"),
+	vrb.img = $("#bodyImg"), vrb.title= $("title"), vrb.boxMd=$(".box-md");
+	populateNav();
+	populatePage(pages[0]);
 });
+
+function changePage(link){
+	fadeOut(300, function(){
+		populatePage(
+			getPage(link.id)
+		);
+	});
+}
+function fadeOut(time, callback){
+	vrb.boxMd.fadeOut(time);
+	setTimeout(function(){callback();},time-10)
+	setTimeout(function(){
+		vrb.boxMd.fadeIn(time)
+	},time+10);
+}
+function getPage(pageName){
+	for (let i in pages)
+		if (pages[i].name == pageName)
+			return pages[i];
+}
+function populatePage(page){
+	vrb.title.html("Teddybear Studio - " + page.name);
+	vrb.headline.html(makeTag(page.headline));
+	var html ="";
+	for (let i in page.body)
+		html += makeTag(page.body[i]);
+	vrb.body.html(html);
+	changeImage(page.img);
+}
+function makeTag(tag){
+	function getId(id){
+		return " id='"+id+"'";
+	}
+	function getClass(classes){
+		return "class='"+classes+"'";
+	}
+	if(tag.type=="a")
+		return;
+	else if(tag.type=="button")
+		return "<"+tag.type+getId(tag.content)+getClass("navButton")+"onclick='changePage(this)'>"+tag.content+"</"+tag.type+">";
+	return "<"+tag.type+">"+tag.content+"</"+tag.type+">";
+}
+function changeImage(img){
+	console.log(vrb.img.attr("src"));
+	vrb.img.attr("src",encodeURI(img.src));
+	vrb.img.attr("alt",img.alt);
+	console.log(vrb.img.attr("src"));
+}
+function populateNav(){
+	var nav = $("#nav");
+	var html ="";
+	for (let i in pages){
+		html+= makeTag({content:pages[i].name,type:"button"});
+	}
+	nav.html(html);
+}
